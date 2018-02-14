716f89d4c03dc6786467ecd3b8beebc5c53b1c1e
marccc <cyc0990@163.com>
2018-02-14 15:46:19 +0800

    集成cloud-bus使用RabbitMQ作为消息队列

--- clound-config-server/src/main/java/com/config/CloudConfigApplication.java
+++ clound-config-server/src/main/java/com/config/CloudConfigApplication.java
@@ -2,11 +2,13 @@ package com.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.ConfigServerApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudConfigApplication {
 
     public static void main(String[] args) {
