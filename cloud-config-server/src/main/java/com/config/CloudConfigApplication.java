1fa4fe4b608e8dc61b101b154a5bc4039e97045f
marccc <cyc0990@163.com>
2018-02-19 15:25:47 +0800

    增加cloud-security应用

--- cloud-config-server/src/main/java/com/config/CloudConfigApplication.java
+++ cloud-config-server/src/main/java/com/config/CloudConfigApplication.java
@@ -14,4 +14,5 @@ public class CloudConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudConfigApplication.class, args);
     }
+
 }
--- cloud-gateway-zuul/src/main/java/com/gateway/GatewayApplication.java
+++ cloud-gateway-zuul/src/main/java/com/gateway/GatewayApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+/**
+ * 网关启动应用
+ *
+ * @author cc
+ */
 @SpringBootApplication
 @EnableZuulProxy
 public class GatewayApplication {
-
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
